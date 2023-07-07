d286cfb89ef50b524d63477685caaca76ca51ca9
Ivan Karzhonov <vanes993636@gmail.com>
2023-07-07 21:23:06 +0700

    add DZ

---
+++ task50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+// 1, 2 -> 2
+
+
+
+int[,] GetRandomArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); ++i)
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+    return matrix;
+}
+
+void PrintMatrixInts(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); ++i)
+    {
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RequestNumberFromArray (int iPosition, int jPosition, int[,] MyMatrix)
+{
+    if (iPosition <= MyMatrix.GetLength(0)-1)
+    {
+        if (jPosition <= MyMatrix.GetLength(1)-1)
+        {
+            Console.Write($"нужное вам число {MyMatrix[iPosition,jPosition]}");
+        }
+        else
+        {
+            Console.Write("такого столбца в массиве нет");
+        }
+    }
+    else
+    {
+        Console.Write("такой строки в массиве нет");
+    }
+}
+
+int rows = 5; 
+int columns = 5;
+Console.Write("введите номер в строке: ");
+int iPosition = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите номер столбика: ");
+int jPosition = Convert.ToInt32(Console.ReadLine());
+int[,] MyMatrix = GetRandomArray(rows, columns);
+PrintMatrixInts (MyMatrix);
+RequestNumberFromArray (iPosition,jPosition, MyMatrix);
---
+++ task52/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,7; 5,7; 3,7; 3.
+
+int[,] GetRandomArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); ++i)
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+    return matrix;
+}
+
+void PrintMatrixInts(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); ++i)
+    {
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnArithmeticMean(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); ++j)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); ++i)
+        {
+            sum += matrix[i,j];
+        }
+        double result = sum/matrix.GetLength(0);
+        Console.Write($"{result:f1} | ");
+    }
+}
+
+int rows = 3; 
+int columns = 3;
+int[,] MyMatrix = GetRandomArray(rows, columns);
+PrintMatrixInts (MyMatrix);
+ColumnArithmeticMean (MyMatrix);
